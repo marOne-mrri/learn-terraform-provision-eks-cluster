ed27712ae42e616934f528f9a2149012afe14938
marOne-mrri <hello@mar0ne.com>
2023-03-02 13:21:02 +0100

    edited the frontend app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,9 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  url: string = '';
   title = 'frontend';
   constructor(private http: HttpClient) {
-    this.http.get("http://localhost:8080").subscribe((data: any) => {
+    // this.http.get("http://localhost:8080").subscribe((data: any) => {
+    //   this.title = data.message
+    // })
+  }
+  onClick() {
+    this.http.get(this.url).subscribe((data: any) => {
       this.title = data.message
     })
   }
